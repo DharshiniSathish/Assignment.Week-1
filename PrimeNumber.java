1c21a8dbff1eb46b00e05c1498cef72148342165
DharshiniSathish <109073477+DharshiniSathish@users.noreply.github.com>
2022-07-12 14:24:35 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -5,16 +5,15 @@ public class PrimeNumber {
 		
 	public static void main(String[] args) {
 		int a = 13;
-		float i = 2f;
-		boolean flag ;
-		for(i=2;i<=(a/2);i++ ) {
-			flag = true; 
-		}if(flag = true) {
-			System.out.println("Given Number is a Prime Number");
-		}
-		else
-		{
-		  System.out.println("Given Number is not a Prime Number");		
+		boolean flag = false;
+		for(int i=2;i<=(a/2);i++ ) {
+			if(a%i == 0){
+				flag = true;
+				break;}}
+			if (flag == false){
+				System.out.println("Given Number " + a +" is a Prime Number");
+			}else {
+			  System.out.println("Given Number is " + a +" not a Prime Number");		
 			}
 		}
 	}
